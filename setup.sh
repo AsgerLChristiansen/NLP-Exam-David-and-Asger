18e81b529d226c247324f78395e8c8bf9617a51f
au639194 <54856779+au639194@users.noreply.github.com>
2022-11-28 18:42:22 +0100

    Add files via upload
    Hope this doesn't fuck up stuff - added a setup file with downloads to ucloud
    # run from terminal - shell script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pip install datasets
+pip install transformers
+pip install itertools 
+pip install typing 
+
+pip install argparse
+pip install numpy
+pip install torch
+
+pip install gensim
+pip install random
